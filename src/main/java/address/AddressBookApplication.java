ba0cb11d02b058976df100a650511671edf80b8c
OverNuke <ksfgarcia24@gmail.com>
2024-05-26 22:23:06 -0600

    Eleventh commit - JavaDocs

--- src/main/java/address/AddressBookApplication.java
+++ src/main/java/address/AddressBookApplication.java
@@ -5,9 +5,9 @@ import java.io.IOException;
  * @author Kevin Sebastián Frias García
  * Class in charge to start the application
  */
-public class AddressBookAplication extends Menu{
+public class AddressBookApplication extends Menu{
     public static void main(String[] args) throws IOException {
-        Menu StartApp = new AddressBookAplication();
+        Menu StartApp = new AddressBookApplication();
         StartApp.displayMenu();
     }
 }
